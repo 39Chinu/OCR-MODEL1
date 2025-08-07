9993b965c4799c5de981b25e92f11dfead74dfe8
39Chinu <153605374+39Chinu@users.noreply.github.com>
2025-08-07 21:00:34 +0530

    marker.py

---
+++ marker.py
@@ -0,0 +1,5 @@
+!pip install marker-pdf torchvision
+#for single pdf conversion -- marker_single pdf path
+!marker_single 2507.17170v1.pdf
+#for multi pdf conversion -- marker /path/to/input/folder
+!marker content/pdfs/
